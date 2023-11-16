37ea2a05ffce9829cff3a4a1726b59356bb303f3
Sponton <134696651+sofisponton@users.noreply.github.com>
2023-11-16 20:23:30 +0000

    commit3

--- src/screens/Main/index.jsx
+++ src/screens/Main/index.jsx
@@ -1,45 +1,78 @@
-import { Box, Center, FormControl, FormControlLabel, FormControlLabelText, HStack, Heading, Input, InputField, Switch, Text } from "@gluestack-ui/themed";
+import { Box, Button, ButtonText, Center, FormControl, FormControlLabel, FormControlLabelText, HStack, Heading, Image, Input, InputField, Switch, Text } from "@gluestack-ui/themed";
+import { useState } from "react";
+
+import homem from "../../../assets/homem-icon.png"
+import mulher from "../../../assets/mulher-icon.png"
+
 
 export default function Main() {
-   
-    return  <Box bg="$primary100" p="$2" h={300} w={300} borderRadius={"$md"}>
-         <Center h={"$full"}>
+    const [peso, setPeso] = useState("72")
+    const [altura, setAltura] = useState("1.70")
+    const [sexo, setSexo] = useState(false)
+    const [resultado, setResultado] = useState("")
+
+    const calcularHandle = () => {
+        let alturaConvertida = parseFloat(altura)
+        let result  = 0
+
+        if (altura>2.2) {
+            setResultado("Altura deve ser menor que 2.2")
+            return;
+        }
+
+        if (sexo) {
+            result = (62.1*alturaConvertida)-44.7
+        } else {
+            result = (72.7*alturaConvertida)-58
+        }
+        
+
+        setResultado(Math.round(result))
+    }
+
+    const limparHandle = () => {
+        setPeso("72")
+        setAltura("1.70")
+        setSexo(false)
+        setResultado("")
+    }
+
+    return <Box bg="$primary100" p="$2" h={"$3/4"} w={300} borderRadius={"$md"}>
+        <Center h={"$full"}>
             <Heading>Peso Ideal</Heading>
             <FormControl>
                 <FormControlLabel>
-                    <FormControlLabelText>
-                        <Text>Peso</Text>
-                    </FormControlLabelText>
+                    <FormControlLabelText>Peso</FormControlLabelText>
                 </FormControlLabel>
-              <Input w={"$full"}>
-              <InputField />
-              </Input>
-              <FormControlLabel>
-                    <FormControlLabelText>
-                        <Text>Altura</Text>
-                    </FormControlLabelText>
+                <Input w={"$full"}>
+                    <InputField value={peso} onChangeText={setPeso} keyboardType="numeric"/>
+                </Input>
+                <FormControlLabel>
+                    <FormControlLabelText>Altura</FormControlLabelText>
                 </FormControlLabel>
-              <Input w={"$full"}>
-              <InputField />
-              </Input>
-
-              <FormControlLabel>
-                    <FormControlLabelText>
-                        <Text>Sexo</Text>
-                    </FormControlLabelText>
+                <Input w={"$full"}>
+                    <InputField value={altura} onChangeText={setAltura} keyboardType="number-pad"/>
+                </Input>
+                <FormControlLabel>
+                    <FormControlLabelText>Sexo</FormControlLabelText>
                 </FormControlLabel>
-              <Input w={"$full"}>
-              <InputField />
-              </Input>
-
-              <HStack w={"$full"} space="md">
-                <Text size="sm">Homem</Text>
-                <Switch/>
-                <Text size="sm">Mulher</Text>
-
-              </HStack>
-
+                <HStack w={"$full"} space="md" justifyContent="center" alignItems="center">
+                    <FormControlLabelText>Homem</FormControlLabelText>
+                    <Switch value={sexo} onValueChange={setSexo} />
+                    <FormControlLabelText>Mulher</FormControlLabelText>
+                </HStack>
+                <Button onPress={calcularHandle}>
+                    <ButtonText>Calcular</ButtonText>
+                </Button>
+                <Button onPress={limparHandle}>
+                    <ButtonText>Limpar</ButtonText>
+                </Button>
+                <Box justifyContent="space-between" alignItems="center" height={100} flexDirection="row">
+                    <Text>Resultado</Text>
+                    <Text fontWeight="$bold">{resultado}</Text>
+                </Box>
             </FormControl>
-         </Center>
-         </Box>
+            <Image source={sexo ? mulher : homem} alt="imagem do tipo do sexo"/>
+        </Center>
+    </Box>
 }
